a372b51fbde34977f3f833a2828923125673fa91
lek34 <97287300+lek34@users.noreply.github.com>
2023-09-19 20:04:33 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <h1>Selamat Datang <br>
+            di Website </h1>
+        <h2>Daniel Pannata</h2>
+        <a href="profil">ke halaman Profil</a>
+    </body>
+</html>
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Profil Saya</title>
+    </head>
+    <body>
+        <h1>Profil Biodata</h1>
+        <h2>Nama  : Daniel Pannata</h2>
+        <h3>NIM   : 03081210006</h3>
+        <h4>Kelas : 21SI2</h4>
+        <h5>Jurusan : Sistem Informasi</h5>
+    </body>
+<html>
