345ecdcdc8e4eab52d35b6ad29ecd370311ed1c7
Daniel-Pannata <106649757+Daniel-Pannata@users.noreply.github.com>
2023-10-11 18:04:28 +0700

    Add files via upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,16 @@
-<html>
-    <head></head>
-    <body>
-        <h1>Selamat Datang <br>
-            di Website </h1>
+@extends('layouts.app2')
+
+@section('title','Home')
+@section('br1','Selamat Datang')
+@section('br2','Selamat Datang')
+@section('content')
+   
+    <center>
+        <h3>Selamat Datang <br>
+            di Website </h3>
         <h2>Daniel Pannata</h2>
         <a href="profil">ke halaman Profil</a>
-    </body>
-</html>
+    </center>
+
+@endsection    
+
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,12 +1,84 @@
-<html>
-    <head>
-        <title>Profil Saya</title>
-    </head>
-    <body>
-        <h1>Profil Biodata</h1>
-        <h2>Nama  : Daniel Pannata</h2>
-        <h3>NIM   : 03081210006</h3>
-        <h4>Kelas : 21SI2</h4>
-        <h5>Jurusan : Sistem Informasi</h5>
-    </body>
-<html>
+@extends('layouts.app2')
+<head>
+    <style>
+        th {
+            border: 1px solid;
+            text-align: center;
+            padding: 12px;
+        }
+        td {
+            border: 1px solid;
+            text-align: center;
+        }
+        p{
+            margin: 10;
+        }
+    </style>
+</head>
+
+@section('title', 'Profil')
+@section ('content')
+<body>
+    <h1>Profile</h1>
+    <div class="table-container">
+        <p> Nama        : Daniel Pannata </p>
+        <p> Umur        : 20 Tahun </p>
+        <p> Student ID  : 03081210004</p>
+        <p> Email       : danielpannata1@gmail.com </p>
+        <p> Kelas       : 21SI2</p>
+        <p> Alamat      : </p>
+        <p> Universitas : Universitas Pelita Harapan</p>
+    </div>
+    <h2 align="left">Matakuliah</h2>
+    <table border="1" width="700px" cellspacing="0">
+        <thead>
+            <tr>
+                <th>Hari</th>
+                <th>Jam</th>
+                <th>Matakuliah</th>
+                <th>Dosen</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Senin</td>
+                <td>17:30 - 20:10</td>
+                <td>Accounting and Finance</td>
+                <td>Ms Lisa</td>
+            </tr>
+            <tr>
+                <td>Selasa</td>
+                <td>17:30 - 21:00</td>
+                <td>Bisnis Aplikasi Programing</td>
+                <td>Ade Maulana</td>
+            </tr>
+            <tr>
+                <td>Rabu</td>
+                <td>17:30 - 20:10</td>
+                <td>Manajemen Strategi Perusahaan</td>
+                <td>Romindo</td>
+            </tr>
+            <tr>
+                <td>Kamis</td>
+                <td>17:30 - 21:00</td>
+                <td>Audit dan Kontrol Sistem Informasi</td>
+                <td>Ade Maulana</td>
+            </tr>
+            <tr>
+                <td>Jumat</td>
+                <td>17:30 - 20:10</td>
+                <td>Digital Marketing</td>
+                <td>Okky Barus</td>
+            </tr>
+            <tr>
+                <td>Sabtu</td>
+                <td>13:00 - 15:00</td>
+                <td>Sistem Informasi Perusahaan</td>
+                <td>Ade Maulana</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
+
+@endsection
